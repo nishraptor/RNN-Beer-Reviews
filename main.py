92a7fa4e7e74c84dd0548c20c5197e384969907d
nishraptor <nmysore@ucsd.edu>
2018-11-23 21:10:54 -0800

    Fixed the not recognized values

--- main.py
+++ main.py
@@ -12,13 +12,19 @@ from nltk.translate import bleu_score
 
 # Functions we needed to implement to one-hot encode the vectors
 def char2oh(str):
-    alphabet  = """abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789&-\",:$%!();.[]?+/' """
+    alphabet  = """abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789&-\",:$%!();.[]?+/'{}@ """
+    for i in range(len(str)):
+        if str[i] not in alphabet:
+            s = list(str)
+            s[i] = '@'
+            str = "".join(s)
+
     vector = [[0 if char != letter else 1 for char in alphabet]
               for letter in str]
     return vector
 
 def oh2char(vector):
-    alphabet  = """abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789&-\",:$%!();.[]?+/' """
+    alphabet  = """abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789&-\",:$%!();.[]?+/'{}@ """
     str = ''
     for i in vector:
         for j in range(len(i)):
