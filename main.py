58eab0cf45f9b5b7bc66714bdc220c2c58d3381b
Zeppelin-V <zvanbarr@ucsd.edu>
2018-11-27 15:14:58 -0800

    Optimizing process_train_data with torch.tensor

--- main.py
+++ main.py
@@ -155,7 +155,9 @@ def pad_data(orig_data):
     # representation in one hot encoding.
 
     max_len = len(max(orig_data, key=len))
-
+    print(orig_data[0].shape)
+    print(np.repeat(char2oh('}'), max_len - orig_data[0].shape[0], axis=0))
+    
 
 
     array_list = [np.append(array, np.repeat(char2oh('}'), max_len - array.shape[0], axis=0), axis=1) for array in orig_data]
