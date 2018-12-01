43f47977ffb5d44b9b5bc3f37ef6901dd3256572
nishraptor <nmysore@ucsd.edu>
2018-12-01 10:30:16 -0800

    Testing Proccessing functions

--- main.py
+++ main.py
@@ -223,7 +223,7 @@ def train(model, data, val_index, cfg,computing_device):
             input, target = process_train_data(minibatch_df, beer_styles, computing_device)
             print(input.size())
             print(input[-1,:,:])
-            print(target[-1,:,:])
+            print(target.size())
             exit()
 
             input, target = input.to(computing_device), target.to(computing_device)
